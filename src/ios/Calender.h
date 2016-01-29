be118bf1103396d606f1c3a598edb310f19ca6a2
gurinder <gurinder@headerlabs.com>
2016-01-29 13:24:52 +0530

    alert pop-up added

---
+++ src/ios/Calender.h
@@ -0,0 +1,10 @@
+/********* Calender.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface Calender : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
